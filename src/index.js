1234a9c01f020134662e33e2906520e5af8b9a7e
vovantais <vladimir0297@mail.ru>
2020-10-10 20:37:05 +0300

    init:start and finish reverse algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = function reverse(num) {
+	let newNum = '';
+	if (num > 0) {
+		return +(num.toString().split('').reverse().join(''));
+	}
+	if (num < 0) {
+		newNum = num.toString().slice(1);
+		return +(newNum.split('').reverse().join(''));
+	}
 }
